c2bd00af2006b81e153feeb8218c9d9f43f7069b
Free System <mrsl8898@gmail.com>
2021-01-02 14:59:05 +0530

    Navigation Drawer

--- app/src/main/java/com/example/tablayoutnew/MainActivity.java
+++ app/src/main/java/com/example/tablayoutnew/MainActivity.java
@@ -7,12 +7,16 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.tablayoutnew.ui.main.SectionsPagerAdapter;
 
@@ -30,6 +34,32 @@ public class MainActivity extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
         tabs.setTabGravity(3);
 
+        DrawerLayout drawerLayout;
+        drawerLayout=findViewById(R.id.drawer_layout);
+
+        Toolbar toolbar;
+        toolbar=findViewById(R.id.toolbar);
+       setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.openmenu);
+        getSupportActionBar().setTitle("  Status Downloader");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!drawerLayout.isDrawerOpen(Gravity.START)) drawerLayout.openDrawer(Gravity.START);
+                else drawerLayout.closeDrawer(Gravity.END);
+
+            }
+        });
+
+       toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+           @Override
+           public boolean onMenuItemClick(MenuItem item) {
+
+               Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
+               return false;
+           }
+       });
 
     }
 }
